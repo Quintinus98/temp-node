c9b652b1a7c22ce6fd87513defa515e9d335a2bb
Obodo David <obododavidsop@gmail.com>
2022-06-13 20:20:12 +0100

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// __dirname = path to current directory
+// __filename = file name
+// module
+// require
+
+console.log(__dirname)
+console.log(__filename)
+
+setInterval(() => {
+  console.log("hello world")
+}, 1000)
